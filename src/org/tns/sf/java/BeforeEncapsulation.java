49cc07dbcb8a205accaac4e1d58cfd686bb79394
akshayshama <akshayhonnappa04@gmail.com>
2025-08-11 19:43:21 +0530

    second commit

---
+++ src/org/tns/sf/java/BeforeEncapsulation.java
@@ -0,0 +1,35 @@
+package org.tns.sf.java;
+
+class Human{
+	private int age;
+	private String name;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+		
+}
+
+public class BeforeEncapsulation {
+
+	public static void main(String[] args) {
+		Human obj = new Human();
+		obj.setAge(87);
+		obj.setName("James");
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+		
+
+	}
+
+}
